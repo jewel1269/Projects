12aeaee218169f2cc559e47648eee0d6c0addb5e
Jewel mia <you@example.com>
2024-02-16 02:42:34 +0600

    first commit

---
+++ style/style.js
@@ -0,0 +1,39 @@
+
+    // const primaryButton = document.getElementById('all-btn');
+    const applyCoupon = document.getElementById('apply-coupon');
+    const flowChart = document.getElementById('flow-chart')
+    const totalPrice = document.getElementById('total-price');
+    const discountPrice = document.getElementById('discount-price');
+    const totalCost = document.getElementById('total');
+
+
+
+
+
+// function keyPress(){
+// const findOut = document.getElementById('card-title1');
+// console.log(findOut)
+
+// }
+
+
+
+
+function buttonPressed(){
+   
+
+
+const addPrice = document.getElementById('total-price').innerText; 
+const changesNumba = parseInt(addPrice);
+
+const pTag = document.getElementById('spone').innerText;
+const changes = parseInt(pTag)
+
+const sum = changes + changesNumba;
+
+const added = document.getElementById('total-price');
+setAttribute(sum)
+
+}
+
+
