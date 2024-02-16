494b054e23d1e22394cd0b5f553c241522eb45c1
Jewel mia <you@example.com>
2024-02-16 23:06:15 +0600

    final projects

--- style/style.js
+++ style/style.js
@@ -1,39 +1,84 @@
 
-    // const primaryButton = document.getElementById('all-btn');
-    const applyCoupon = document.getElementById('apply-coupon');
-    const flowChart = document.getElementById('flow-chart')
-    const totalPrice = document.getElementById('total-price');
-    const discountPrice = document.getElementById('discount-price');
-    const totalCost = document.getElementById('total');
-
-
-
-
-
-// function keyPress(){
-// const findOut = document.getElementById('card-title1');
-// console.log(findOut)
-
-// }
-
-
-
-
-function buttonPressed(){
    
+    
+    const flowChart = document.getElementById('flow-chart')
+    
+    const discountPrice = document.getElementById('discount-price');
+    
 
 
-const addPrice = document.getElementById('total-price').innerText; 
-const changesNumba = parseInt(addPrice);
 
-const pTag = document.getElementById('spone').innerText;
-const changes = parseInt(pTag)
+const allButton = document.getElementsByClassName('btn btn-primary');
+let count = 0;
+for(const btn of allButton){
+    btn.addEventListener('click', function(e){
+        count = count + 1;
+        const placeName = e.target.parentNode.parentNode.childNodes[1].innerText;
+       const price = e.target.parentNode.parentNode.childNodes[4].innerText;
+       const convertedPrice = parseInt(price)
+       console.log(convertedPrice)
+   
+  
+    
+    //    const addPrice = document.getElementById('flow-chart').innerText = placeName;
+    // //    const addPrice = document.getElementById('flow-chart').innerText = price;
+const selectContainer = document.getElementById('flow-chart');
+const li = document.createElement('li');
+const p = document.createElement('p')
+p.innerText = placeName;
+const p2 = document.createElement('p')
+p2.innerText = price;
+      li.appendChild(p);
+       li.appendChild(p2);
+       selectContainer.appendChild(li);
 
-const sum = changes + changesNumba;
 
-const added = document.getElementById('total-price');
-setAttribute(sum)
+       const totalCost = document.getElementById('total-price').innerText;
+     const convertedTotalCost = parseInt(totalCost);
+     const outShow = convertedPrice + convertedTotalCost;
+    // console.log(totalCost)
+    
+     
 
+       
+     document.getElementById('total-price').innerText= outShow;
+     const finalAmount =  document.getElementById('total').innerText= outShow;
+     console.log(finalAmount)
+})
 }
+
+const applyCoupon = document.getElementById('apply-coupon');
+applyCoupon.addEventListener("click", function(){
+    
+const inputArea = document.getElementById('input-field').value;
+     const couponCode = inputArea.split(" ").join("").toUpperCase();
+
+if(finalAmount >= 200){
+     if(couponCode === "SELL200"){
+        const inputField = document.getElementById('discount-price')
+      const discountAmount = finalAmount * 0.2;
+      inputField.innerText = discountAmount;
+     }
+     else{
+        alert("Invalid Coupon");
+     }
+    }
+    else{
+        alert("Please fill up 200$")
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
